4258259a96f63c5d1959ae5c2534e4369d50ac47
benjenkinsiv <benjenkinsiv@gmail.com>
2014-06-08 18:04:44 -0500

    add plot 4

---
+++ plot4.R
@@ -0,0 +1,41 @@
+plot4 <- function(){
+# download file
+require(RCurl)
+bin <- getBinaryURL("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",ssl.verifypeer=FALSE)
+con <- file("exdata-data-household_power_consumption.zip", open = "wb")
+writeBin(bin, con)
+close(con)
+
+# unzip to local file
+unzip("exdata-data-household_power_consumption.zip", "household_power_consumption.txt", list = FALSE, overwrite = TRUE)
+
+# load zipped file
+unzipped <- read.csv(file="household_power_consumption.txt", header = TRUE, sep = ";", quote = "\"",
+                     dec = ".", fill = TRUE, comment.char = "")
+
+# use SQL to find date range 
+library(sqldf)
+# add column to convert to Date SQL can understand
+unzipped$newDate <- as.character(as.Date(as.character(unzipped$Date), format = "%d/%m/%Y")) 
+# create dataframe with date constraint
+x<-sqldf("select * FROM unzipped WHERE newDate BETWEEN '2007-02-01' and '2007-02-02' ")
+# add column converting newDate and Time 
+x$datetime <- strptime(paste(x$newDate, x$Time),"%Y-%m-%d %H:%M:%S")
+# create plots
+par(mfrow = c(2, 2), mar = c(4, 3, 1, 1), oma = c(0, 0, 2, 0))
+with(x, {
+  plot( x$datetime, as.numeric(levels(x$Global_active_power))[x$Global_active_power],type="l",main='',xlab="",ylab="Global Active Power" )
+  plot( x$datetime, as.numeric(levels(x$Voltage))[x$Voltage],type="l",main='',xlab="datetime",ylab="Voltage" )
+  plot( x$datetime, as.numeric(levels(x$Sub_metering_1))[x$Sub_metering_1],type="l", main='',xlab="",ylab="Energy sub metering",ylim=c(0,35), yaxt="n")
+  axis(side = 2, at = c(0,10,20,30), c("0","10", "20", "30"))
+  lines(x$datetime, as.numeric(levels(x$Sub_metering_1))[x$Sub_metering_1])
+  lines(x$datetime, as.numeric(levels(x$Sub_metering_2))[x$Sub_metering_2], col="red")
+  lines(x$datetime, x$Sub_metering_3, col="blue")
+  legend("topright",legend=c("sub_metering_1","sub_metering_2","sub_metering_3"),col=c("black","red","blue"),lty=c(1,1,1))
+  plot( x$datetime, as.numeric(levels(x$Global_reactive_power))[x$Global_reactive_power],type="l",main='',xlab="datetime",ylab="Global Reactive Power" )
+  
+})
+#creat png
+dev.copy(png, file = "plot4.png")
+dev.off()
+}
